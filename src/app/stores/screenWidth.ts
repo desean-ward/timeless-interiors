abaf947c8e3a850ac4871383bba08f49eef95040
De Sean Ward <d.ward.chi@gmail.com>
2025-01-09 08:18:01 -0500

    Fix to Zustand local storage call

--- src/app/stores/screenWidth.ts
+++ src/app/stores/screenWidth.ts
@@ -2,17 +2,16 @@ import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
 const useScreenWidth = create(
-    persist(
-        (set) => ({
-        screenWidth: window.innerWidth,
-        setScreenWidth: (width: number) => set({ screenWidth: width }),
-        }),
-        {
-            name: "screenWidth-storage",
-            storage: () => localStorage,
-            storage: createJSONStorage(() => localStorage),
-        }
-    )
-    );
+  persist(
+    (set) => ({
+      screenWidth: window.innerWidth,
+      setScreenWidth: (width: number) => set({ screenWidth: width }),
+    }),
+    {
+      name: "screenWidth-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
 
 export default useScreenWidth;
